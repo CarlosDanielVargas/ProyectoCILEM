01f5de34d34bf4bc2407ac2d5deec4d7c2a94a7e
Carlos Daniel Vargas Varela <93839690+CarlosDVargas@users.noreply.github.com>
2023-06-17 15:12:13 -0600

    Se agrega filtrado en la vista de listar menores

--- UI/frmMinorList.Designer.vb
+++ UI/frmMinorList.Designer.vb
@@ -24,6 +24,16 @@ Partial Class frmMinorList
 	Private Sub InitializeComponent()
         Me.lbIndications = New System.Windows.Forms.Label()
         Me.dgvMinors = New System.Windows.Forms.DataGridView()
+        Me.lbFilters = New System.Windows.Forms.Label()
+        Me.lbIDCard = New System.Windows.Forms.Label()
+        Me.tbIDCard = New System.Windows.Forms.TextBox()
+        Me.lbLevel = New System.Windows.Forms.Label()
+        Me.cboxLevel = New System.Windows.Forms.ComboBox()
+        Me.cboxGender = New System.Windows.Forms.ComboBox()
+        Me.lbGender = New System.Windows.Forms.Label()
+        Me.btnFilter = New System.Windows.Forms.Button()
+        Me.btnClearFilters = New System.Windows.Forms.Button()
+        Me.lbCurrentFilter = New System.Windows.Forms.Label()
         CType(Me.dgvMinors, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -40,18 +50,124 @@ Partial Class frmMinorList
         'dgvMinors
         '
         Me.dgvMinors.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvMinors.Location = New System.Drawing.Point(12, 72)
+        Me.dgvMinors.Location = New System.Drawing.Point(12, 159)
         Me.dgvMinors.Name = "dgvMinors"
         Me.dgvMinors.RowHeadersWidth = 51
         Me.dgvMinors.RowTemplate.Height = 24
-        Me.dgvMinors.Size = New System.Drawing.Size(855, 592)
+        Me.dgvMinors.Size = New System.Drawing.Size(855, 505)
         Me.dgvMinors.TabIndex = 7
+        '
+        'lbFilters
+        '
+        Me.lbFilters.AutoSize = True
+        Me.lbFilters.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!)
+        Me.lbFilters.Location = New System.Drawing.Point(12, 52)
+        Me.lbFilters.Name = "lbFilters"
+        Me.lbFilters.Size = New System.Drawing.Size(182, 24)
+        Me.lbFilters.TabIndex = 8
+        Me.lbFilters.Text = "Filtros de búsqueda:"
+        '
+        'lbIDCard
+        '
+        Me.lbIDCard.AutoSize = True
+        Me.lbIDCard.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!)
+        Me.lbIDCard.Location = New System.Drawing.Point(13, 82)
+        Me.lbIDCard.Name = "lbIDCard"
+        Me.lbIDCard.Size = New System.Drawing.Size(72, 22)
+        Me.lbIDCard.TabIndex = 9
+        Me.lbIDCard.Text = "Cédula:"
+        '
+        'tbIDCard
+        '
+        Me.tbIDCard.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!)
+        Me.tbIDCard.Location = New System.Drawing.Point(90, 82)
+        Me.tbIDCard.Name = "tbIDCard"
+        Me.tbIDCard.Size = New System.Drawing.Size(184, 28)
+        Me.tbIDCard.TabIndex = 10
+        '
+        'lbLevel
+        '
+        Me.lbLevel.AutoSize = True
+        Me.lbLevel.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!)
+        Me.lbLevel.Location = New System.Drawing.Point(290, 82)
+        Me.lbLevel.Name = "lbLevel"
+        Me.lbLevel.Size = New System.Drawing.Size(55, 22)
+        Me.lbLevel.TabIndex = 11
+        Me.lbLevel.Text = "Nivel:"
+        '
+        'cboxLevel
+        '
+        Me.cboxLevel.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!)
+        Me.cboxLevel.FormattingEnabled = True
+        Me.cboxLevel.Location = New System.Drawing.Point(344, 82)
+        Me.cboxLevel.Name = "cboxLevel"
+        Me.cboxLevel.Size = New System.Drawing.Size(121, 30)
+        Me.cboxLevel.TabIndex = 12
+        '
+        'cboxGender
+        '
+        Me.cboxGender.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!)
+        Me.cboxGender.FormattingEnabled = True
+        Me.cboxGender.Location = New System.Drawing.Point(554, 80)
+        Me.cboxGender.Name = "cboxGender"
+        Me.cboxGender.Size = New System.Drawing.Size(121, 30)
+        Me.cboxGender.TabIndex = 14
+        '
+        'lbGender
+        '
+        Me.lbGender.AutoSize = True
+        Me.lbGender.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!)
+        Me.lbGender.Location = New System.Drawing.Point(479, 80)
+        Me.lbGender.Name = "lbGender"
+        Me.lbGender.Size = New System.Drawing.Size(75, 22)
+        Me.lbGender.TabIndex = 13
+        Me.lbGender.Text = "Género:"
+        '
+        'btnFilter
+        '
+        Me.btnFilter.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!)
+        Me.btnFilter.Location = New System.Drawing.Point(725, 80)
+        Me.btnFilter.Name = "btnFilter"
+        Me.btnFilter.Size = New System.Drawing.Size(127, 32)
+        Me.btnFilter.TabIndex = 15
+        Me.btnFilter.Text = "Filtrar"
+        Me.btnFilter.UseVisualStyleBackColor = True
+        '
+        'btnClearFilters
+        '
+        Me.btnClearFilters.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!)
+        Me.btnClearFilters.Location = New System.Drawing.Point(725, 118)
+        Me.btnClearFilters.Name = "btnClearFilters"
+        Me.btnClearFilters.Size = New System.Drawing.Size(127, 32)
+        Me.btnClearFilters.TabIndex = 16
+        Me.btnClearFilters.Text = "Limpiar filtros"
+        Me.btnClearFilters.UseVisualStyleBackColor = True
+        '
+        'lbCurrentFilter
+        '
+        Me.lbCurrentFilter.AutoSize = True
+        Me.lbCurrentFilter.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!)
+        Me.lbCurrentFilter.Location = New System.Drawing.Point(13, 118)
+        Me.lbCurrentFilter.Name = "lbCurrentFilter"
+        Me.lbCurrentFilter.Size = New System.Drawing.Size(64, 22)
+        Me.lbCurrentFilter.TabIndex = 17
+        Me.lbCurrentFilter.Text = "Label1"
         '
         'frmMinorList
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(879, 676)
+        Me.Controls.Add(Me.lbCurrentFilter)
+        Me.Controls.Add(Me.btnClearFilters)
+        Me.Controls.Add(Me.btnFilter)
+        Me.Controls.Add(Me.cboxGender)
+        Me.Controls.Add(Me.lbGender)
+        Me.Controls.Add(Me.cboxLevel)
+        Me.Controls.Add(Me.lbLevel)
+        Me.Controls.Add(Me.tbIDCard)
+        Me.Controls.Add(Me.lbIDCard)
+        Me.Controls.Add(Me.lbFilters)
         Me.Controls.Add(Me.dgvMinors)
         Me.Controls.Add(Me.lbIndications)
         Me.Margin = New System.Windows.Forms.Padding(4)
@@ -66,4 +182,14 @@ Partial Class frmMinorList
 
     Friend WithEvents lbIndications As Label
 	Friend WithEvents dgvMinors As DataGridView
+    Friend WithEvents lbFilters As Label
+    Friend WithEvents lbIDCard As Label
+    Friend WithEvents tbIDCard As TextBox
+    Friend WithEvents lbLevel As Label
+    Friend WithEvents cboxLevel As ComboBox
+    Friend WithEvents cboxGender As ComboBox
+    Friend WithEvents lbGender As Label
+    Friend WithEvents btnFilter As Button
+    Friend WithEvents btnClearFilters As Button
+    Friend WithEvents lbCurrentFilter As Label
 End Class
--- UI/frmMinorList.vb
+++ UI/frmMinorList.vb
@@ -1,94 +1,202 @@
 ﻿Imports DOM
 Imports BL
 Public Class frmMinorList
-	' This form is used to display a list of minors
-	Private minors As List(Of Minor)
+    ' This form is used to display a list of minors
+    Private minors As List(Of Minor)
+    Private levels As List(Of Level)
 
-	' This event handler is called when the form is loaded
-	Private Sub frmMinorList_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-		' Load the minors from the database
-		Dim minorManager As New MinorManager()
-		minors = minorManager.loadAllFromDB()
+    ' This event handler is called when the form is loaded
+    Private Sub frmMinorList_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Load the minors from the database
+        Dim minorManager As New MinorManager()
+        minors = minorManager.loadAllFromDB()
+
+        ' Load the levels from the database
+        Dim levelManager As New LevelManager()
+        levels = levelManager.loadAllFromDB()
+        cboxLevel.DataSource = levels
+        cboxLevel.DisplayMember = "Name"
+        cboxLevel.ValueMember = "LevelID"
+        cboxLevel.SelectedIndex = 0
+
+        ' Load genders
+        cboxGender.DataSource = [Enum].GetValues(GetType(Minor.GenderEnum))
+        cboxGender.SelectedIndex = 0
 
 
-		dgvMinors.ReadOnly = True ' Set the DataGridView to read-only
-		dgvMinors.AllowUserToAddRows = False ' Disable the ability to add new rows
+        dgvMinors.ReadOnly = True ' Set the DataGridView to read-only
+        dgvMinors.AllowUserToAddRows = False ' Disable the ability to add new rows
 
-		' Add the columns to the DataGridView
-		dgvMinors.Columns.Add("Nombre", "Nombre")
-		dgvMinors.Columns.Add("Cédula", "Cédula")
-		dgvMinors.Columns.Add("Nivel", "Nivel")
+        ' Add the columns to the DataGridView
+        dgvMinors.Columns.Add("Nombre", "Nombre")
+        dgvMinors.Columns.Add("Cédula", "Cédula")
+        dgvMinors.Columns.Add("Nivel", "Nivel")
 
-		' Add the buttons column to the DataGridView
-		' This columns will contain the "Detalles", "Eliminar" and "Editar" buttons
-		Dim btnDetallesColumn As New DataGridViewButtonColumn()
-		btnDetallesColumn.HeaderText = "Detalles"
-		btnDetallesColumn.Name = "Detalles"
-		btnDetallesColumn.Text = "Detalles"
-		btnDetallesColumn.UseColumnTextForButtonValue = True
+        ' Add the buttons column to the DataGridView
+        ' This columns will contain the "Detalles", "Eliminar" and "Editar" buttons
+        Dim btnDetallesColumn As New DataGridViewButtonColumn()
+        btnDetallesColumn.HeaderText = "Detalles"
+        btnDetallesColumn.Name = "Detalles"
+        btnDetallesColumn.Text = "Detalles"
+        btnDetallesColumn.UseColumnTextForButtonValue = True
 
-		Dim btnDeleteColumn As New DataGridViewButtonColumn()
-		btnDeleteColumn.HeaderText = "Eliminar"
-		btnDeleteColumn.Name = "Eliminar"
-		btnDeleteColumn.Text = "Eliminar"
-		btnDeleteColumn.UseColumnTextForButtonValue = True
+        Dim btnDeleteColumn As New DataGridViewButtonColumn()
+        btnDeleteColumn.HeaderText = "Eliminar"
+        btnDeleteColumn.Name = "Eliminar"
+        btnDeleteColumn.Text = "Eliminar"
+        btnDeleteColumn.UseColumnTextForButtonValue = True
 
-		Dim btnEditColumn As New DataGridViewButtonColumn()
-		btnEditColumn.HeaderText = "Editar"
-		btnEditColumn.Name = "Editar"
-		btnEditColumn.Text = "Editar"
-		btnEditColumn.UseColumnTextForButtonValue = True
+        Dim btnEditColumn As New DataGridViewButtonColumn()
+        btnEditColumn.HeaderText = "Editar"
+        btnEditColumn.Name = "Editar"
+        btnEditColumn.Text = "Editar"
+        btnEditColumn.UseColumnTextForButtonValue = True
 
-		dgvMinors.Columns.Add(btnDetallesColumn)
-		dgvMinors.Columns.Add(btnDeleteColumn)
-		dgvMinors.Columns.Add(btnEditColumn)
+        dgvMinors.Columns.Add(btnDetallesColumn)
+        dgvMinors.Columns.Add(btnDeleteColumn)
+        dgvMinors.Columns.Add(btnEditColumn)
 
-		' Add the minors to the DataGridView
-		For Each column As DataGridViewColumn In dgvMinors.Columns
-			column.ReadOnly = True ' Set individual columns to read-only
-		Next
+        ' Add the minors to the DataGridView
+        For Each column As DataGridViewColumn In dgvMinors.Columns
+            column.ReadOnly = True ' Set individual columns to read-only
+        Next
 
-		' Add the relationship of the buttons to the Minor object
-		For Each minor As Minor In minors
-			Dim rowIndex As Integer = dgvMinors.Rows.Add()
+        ' Add the relationship of the buttons to the Minor object
+        For Each minor As Minor In minors
+            Dim rowIndex As Integer = dgvMinors.Rows.Add()
 
-			dgvMinors.Rows(rowIndex).Cells("Nombre").Value = minor.Name
-			dgvMinors.Rows(rowIndex).Cells("Cédula").Value = minor.MinorID
-			dgvMinors.Rows(rowIndex).Cells("Nivel").Value = minor.Level.Name
-		Next
-	End Sub
+            dgvMinors.Rows(rowIndex).Cells("Nombre").Value = minor.Name
+            dgvMinors.Rows(rowIndex).Cells("Cédula").Value = minor.MinorID
+            dgvMinors.Rows(rowIndex).Cells("Nivel").Value = minor.Level.Name
+        Next
 
-	Private Sub dgvMinors_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvMinors.CellContentClick
-		' Check if the clicked cell is a button cell and the corresponding column index
-		If e.ColumnIndex = dgvMinors.Columns("Detalles").Index AndAlso e.RowIndex >= 0 Then
-			' "Detalles" button clicked
-			' Get the row index of the clicked cell
-			Dim rowIndex As Integer = e.RowIndex
-			' Get the Minor object from the list of minors using the row index
-			Dim minor As Minor = minors(rowIndex)
-			' Open the form for displaying the minor details using the minor object
-			Dim form As New frmMinorDetails(minor)
-			form.MdiParent = Me.MdiParent
-			form.Show()
-		ElseIf e.ColumnIndex = dgvMinors.Columns("Eliminar").Index AndAlso e.RowIndex >= 0 Then
-			' "Eliminar" button clicked
-			Dim rowIndex As Integer = e.RowIndex
-			Dim minor As Minor = minors(rowIndex)
-			' Perform delete action using the minor object
-			' Example:
-			' minorManager.deleteFromDB(minor.MinorID)
-			' Reload the DataGridView data after deletion
+        ' Set message of filter label
+        lbCurrentFilter.Text = "Se muestran todos los menores, un total de " & minors.Count & " menor(es)"
 
-		ElseIf e.ColumnIndex = dgvMinors.Columns("Editar").Index AndAlso e.RowIndex >= 0 Then
-			' "Editar" button clicked
-			' Get the row index of the clicked cell
-			Dim rowIndex As Integer = e.RowIndex
-			' Get the Minor object from the list of minors using the row index
-			Dim minor As Minor = minors(rowIndex)
-			' Open the form for editing the minor details using the minor object
-			Dim frm As New frmInsertUpdateMinor(minor)
-			frm.MdiParent = Me.MdiParent
-			frm.Show()
-		End If
-	End Sub
+    End Sub
+
+    Private Sub dgvMinors_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvMinors.CellContentClick
+        ' Check if the clicked cell is a button cell and the corresponding column index
+        If e.ColumnIndex = dgvMinors.Columns("Detalles").Index AndAlso e.RowIndex >= 0 Then
+            ' "Detalles" button clicked
+            ' Get the row index of the clicked cell
+            Dim rowIndex As Integer = e.RowIndex
+            ' Get the Minor object from the list of minors using the row index
+            Dim minor As Minor = minors(rowIndex)
+            ' Open the form for displaying the minor details using the minor object
+            Dim form As New frmMinorDetails(minor)
+            form.MdiParent = Me.MdiParent
+            form.Show()
+        ElseIf e.ColumnIndex = dgvMinors.Columns("Eliminar").Index AndAlso e.RowIndex >= 0 Then
+            ' "Eliminar" button clicked
+            Dim rowIndex As Integer = e.RowIndex
+            Dim minor As Minor = minors(rowIndex)
+            ' Perform delete action using the minor object
+            ' Example:
+            ' minorManager.deleteFromDB(minor.MinorID)
+            ' Reload the DataGridView data after deletion
+
+        ElseIf e.ColumnIndex = dgvMinors.Columns("Editar").Index AndAlso e.RowIndex >= 0 Then
+            ' "Editar" button clicked
+            ' Get the row index of the clicked cell
+            Dim rowIndex As Integer = e.RowIndex
+            ' Get the Minor object from the list of minors using the row index
+            Dim minor As Minor = minors(rowIndex)
+            ' Open the form for editing the minor details using the minor object
+            Dim frm As New frmInsertUpdateMinor(minor)
+            frm.MdiParent = Me.MdiParent
+            frm.Show()
+        End If
+    End Sub
+
+    Private Sub btnFilter_Click(sender As Object, e As EventArgs) Handles btnFilter.Click
+        Dim filteredMinors As New List(Of Minor)
+
+        Dim IDFilter As String = tbIDCard.Text
+        Dim levelFilter As Integer = cboxLevel.SelectedValue
+        Dim genderFilter As String = cboxGender.SelectedValue?.ToString()
+
+        ' Filter by ID
+        If Not String.IsNullOrEmpty(IDFilter) Then
+            filteredMinors = filteredMinors.FindAll(Function(minor) minor.MinorID <> IDFilter)
+        End If
+
+        ' Filter by level
+        If levelFilter <> 0 Then
+            filteredMinors = minors.FindAll(Function(minor) minor.Level.LevelID = levelFilter)
+        End If
+
+        ' Filter by gender
+        If Not String.IsNullOrEmpty(genderFilter) Then
+            filteredMinors = filteredMinors.FindAll(Function(minor) minor.Gender = genderFilter)
+        End If
+
+        dgvMinors.Rows.Clear()
+
+        ' Add the relationship of the buttons to the Minor object
+        For Each minor As Minor In filteredMinors
+            Dim rowIndex As Integer = dgvMinors.Rows.Add()
+
+            dgvMinors.Rows(rowIndex).Cells("Nombre").Value = minor.Name
+            dgvMinors.Rows(rowIndex).Cells("Cédula").Value = minor.MinorID
+            dgvMinors.Rows(rowIndex).Cells("Nivel").Value = minor.Level.Name
+        Next
+
+        ' Add the minors to the DataGridView
+        For Each column As DataGridViewColumn In dgvMinors.Columns
+            column.ReadOnly = True ' Set individual columns to read-only
+        Next
+
+        ' Change the filter message
+        Dim filterMessage As String = "Filtrado por: "
+
+        If Not String.IsNullOrEmpty(IDFilter) Then
+            filterMessage += "Cédula: " + IDFilter + ", "
+        End If
+
+        If levelFilter <> 0 Then
+            Dim selectedLevel As Level = levels.FirstOrDefault(Function(l) l.LevelID = levelFilter)
+            If selectedLevel IsNot Nothing Then
+                filterMessage += "Nivel: " + selectedLevel.Name + ", "
+            End If
+        End If
+
+        If Not String.IsNullOrEmpty(genderFilter) Then
+            filterMessage += "Género: " + genderFilter + ", "
+        End If
+
+        If filterMessage.EndsWith(", ") Then
+            filterMessage = filterMessage.Substring(0, filterMessage.Length - 2) ' Remove the last comma and space
+        End If
+
+        lbCurrentFilter.Text = filterMessage
+    End Sub
+
+    Private Sub btnClearFilters_Click(sender As Object, e As EventArgs) Handles btnClearFilters.Click
+        ' Clear the filter values and reset the DataGridView
+
+        ' Clear the filter text boxes
+        tbIDCard.Text = String.Empty
+        cboxLevel.SelectedIndex = -1
+        cboxGender.SelectedIndex = -1
+
+        ' Clear the filter message
+        lbCurrentFilter.Text = "Se muestran todos los menores, un total de " & minors.Count & " menor(es)"
+
+        ' Refresh the DataGridView to show all minors
+        dgvMinors.Rows.Clear()
+
+        For Each minor As Minor In minors
+            Dim rowIndex As Integer = dgvMinors.Rows.Add()
+
+            dgvMinors.Rows(rowIndex).Cells("Nombre").Value = minor.Name
+            dgvMinors.Rows(rowIndex).Cells("Cédula").Value = minor.MinorID
+            dgvMinors.Rows(rowIndex).Cells("Nivel").Value = minor.Level.Name
+        Next
+
+        ' Set all columns to read-only
+        For Each column As DataGridViewColumn In dgvMinors.Columns
+            column.ReadOnly = True
+        Next
+    End Sub
 End Class
